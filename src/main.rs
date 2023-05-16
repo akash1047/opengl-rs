f885f55268c00879f917f3db30372a6b6ab3257c
akash1047 <94453657+akash1047@users.noreply.github.com>
2023-05-16 19:20:01 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use glad_gl::gl;
+use glfw::{Action, Context, Key};
+
+fn main() {
+    let width: i32 = 800;
+    let height: i32 = 800;
+    let title = env!("CARGO_PKG_NAME");
+
+    // initialize glfw library
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    // Create a windowed mode window and its OpenGL context
+    let (mut window, events) = glfw
+        .create_window(
+            width as u32,
+            height as u32,
+            title,
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window.");
+
+    // Make the window's context current
+    window.make_current();
+
+    window.set_key_polling(true);
+
+    // load opengl functions
+    gl::load(|s| window.get_proc_address(s));
+
+    unsafe {
+        gl::ClearColor(1.0f32, 1.0f32, 0.4f32, 0.7f32);
+    }
+
+    // Loop until the user closes the window
+    while !window.should_close() {
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        // Swap front and back buffers
+        window.swap_buffers();
+
+        // Poll for and process events
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        }
+    }
+}
